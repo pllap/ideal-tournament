3664cca5704a0f25afa418acf103004c61ea50d5
pllapallpal <pllapallpal@gmail.com>
2019-12-18 01:09:25 +0900

    Update ListManager

--- src/logic/ListManager.java
+++ src/logic/ListManager.java
@@ -25,30 +25,13 @@ public class ListManager {
     private static int getCutListSize(List<ContentItem> list) {
 
         int size = list.size();
-        if (size <= 1) {
-            return 1;
+
+        int cutSize = 1;
+        while (cutSize < size) {
+            cutSize *= 2;
         }
-        else if (size < 4) {
-            return 2;
-        }
-        else if (size < 8) {
-            return 4;
-        }
-        else if (size < 16) {
-            return 8;
-        }
-        else if (size < 32) {
-            return 16;
-        }
-        else if (size < 64) {
-            return 32;
-        }
-        else if (size < 128) {
-            return 64;
-        }
-        else {
-            return 128;
-        }
+
+        return cutSize/2;
     }
 
 }
